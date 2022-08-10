22f74d62504ff4658e1bfc7e7de6aabaf7945a33
DavidBolaji <odavidbolaji14@gmail.com>
2022-08-10 16:07:04 +0100

    remove router

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import SinglePage from './pages/SinglePage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/single/:id' element={<SinglePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Home />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Home />} />
+    //     <Route path='/single/:id' element={<SinglePage />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
